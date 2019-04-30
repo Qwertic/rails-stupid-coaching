6abc878c7d2910e3d8165d0ea1bc6a43ec88bcfe
Regi Voda <vodaregi@gmail.com>
2019-04-30 17:37:52 +0200

    style changes made

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'ask', to: 'questions#ask', as: :ask
+  get '/ask', to: 'questions#ask', as: :ask
 
-  get 'answer', to: 'questions#answer', as: :answer
+  get '/answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
